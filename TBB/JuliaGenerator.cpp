c126261c8ebc52096de8571d41c61fb825e5f6dc
Cristian Cristea <cristian.cristea@stud.etti.upb.ro>
2024-12-05 20:56:15 +0200

    Remove static const variable

--- TBB/JuliaGenerator.cpp
+++ TBB/JuliaGenerator.cpp
@@ -5,15 +5,13 @@ JuliaGenerator::JuliaGenerator(Size const & imageSize, std::size_t const maxIter
 
 auto JuliaGenerator::generate(Point const & startPoint) const -> std::uint8_t
 {
-    static const auto LOG_MAX_ITERATIONS{std::log(maxIterations)};
-
     auto point{startPoint};
 
     for (std::size_t iteration = 0; iteration < maxIterations; ++iteration)
     {
         if (std::abs(point) > RADIUS)
         {
-            return static_cast<std::uint8_t>(MAX_COLOR * std::log(iteration + 1) / LOG_MAX_ITERATIONS);
+            return static_cast<std::uint8_t>(MAX_COLOR * std::log(iteration + 1) / logMaxIterations);
         }
 
         point = point * point + C_POINT;
